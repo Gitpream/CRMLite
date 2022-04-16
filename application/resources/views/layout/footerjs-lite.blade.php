6e798a740ed49ca91405834faec629f8a4022ec2
Gitpream <premd697@gmail.com>
2022-04-16 16:20:46 -0700

    code init

---
+++ application/resources/views/layout/footerjs-lite.blade.php
@@ -0,0 +1,8 @@
+<!--ALL THIRD PART JAVASCRIPTS-->
+<script src="public/vendor/js/vendor-lite.footer.js?v={{ config('system.versioning') }}"></script>
+
+<!--crmlite.core.js-->
+<script src="public/js/core/ajax.js?v={{ config('system.versioning') }}"></script>
+
+<!--MAIN JS - AT END-->
+<script src="public/js/core/boot-lite.js?v={{ config('system.versioning') }}"></script>
